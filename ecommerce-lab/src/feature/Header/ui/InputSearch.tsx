8859c8ccf96d2474c24322a77e8eabbe27a93cd0
Daniel <danielpapereiraJV@gmail.com>
2025-08-24 18:20:46 -0300

    feat/header - Componente header finalizado

---
+++ ecommerce-lab/src/feature/Header/ui/InputSearch.tsx
@@ -0,0 +1,5 @@
+import { Input } from "@/components/ui/input";
+
+export default function InputSearch(){
+    return <Input className='bg-white' type='text' placeholder='O que você esta procurando ? ...' />
+}
